d10038490cf2623f257f293930898d4e1665a68f
HIROKI KINJO <e135755@HIROKI-no-MacBook-Pro-2.local>
2015-02-20 19:03:21 +0900

    テキストインプットのをつくろうとした。（ほぼコメントアウト）

---
+++ ProjectM_ver2/ProjectM_ver2/TextInputViewController1.swift
@@ -0,0 +1,9 @@
+//
+//  TextInputViewController1.swift
+//  ProjectM_ver2
+//
+//  Created by HIROKI KINJO on 2015/01/10.
+//  Copyright (c) 2015年 TeamM. All rights reserved.
+//
+
+import Foundation
